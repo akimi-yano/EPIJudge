4c007665973f6ddaa091da1f66bf070f8c8a8fab
Akimi Yano <akimi.mimi.yano@gmail.com>
2020-07-27 11:27:19 -0700

    completed

--- epi_judge_python/is_number_palindromic.py
+++ epi_judge_python/is_number_palindromic.py
@@ -1,8 +1,26 @@
 from test_framework import generic_test
 
+import math
 
 def is_palindrome_number(x: int) -> bool:
-    # TODO - you fill in here.
+
+    # if x is a negative number, it's False due to the '-' sign
+    if x <=0:
+        return x == 0
+    
+    # formula to get the number of digits (basically same as int diviging it by 10 until it becomes smaller than 0)
+    num_digits = math.floor(math.log10(x)) + 1 
+    # made a mask to filter to check and remove digits
+    msd_mask = 10**(num_digits - 1)
+
+    for _ in range(num_digits // 2):
+        if x // msd_mask != x % 10: 
+            return False
+    
+        x %= msd_mask # Remove the most significant digit of x 
+        x //= 10 # Remove the least significant digix of x. 
+        # reducing the 2 digits after checking if they are palindrome
+        msd_mask //= 100
     return True
 
 
--- problem_mapping.js
+++ problem_mapping.js
@@ -65,7 +65,7 @@ problem_mapping = {
                 "passed": 0,
                 "total": 10000
             },
-            "Python: closest_int_same_weight.py": { 
+            "Python: closest_int_same_weight.py": {
                 "passed": 0,
                 "total": 10000
             }
@@ -136,7 +136,7 @@ problem_mapping = {
                 "total": 20000
             },
             "Python: is_number_palindromic.py": {
-                "passed": 0,
+                "passed": 20000,
                 "total": 20000
             }
         },
