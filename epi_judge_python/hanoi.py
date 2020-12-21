21f313868f244f969c7ed8fb151095279be0b2cf
Akimi Yano <akimi.mimi.yano@gmail.com>
2020-12-20 17:49:49 -0800

    hanoi

--- epi_judge_python/hanoi.py
+++ epi_judge_python/hanoi.py
@@ -9,8 +9,68 @@ NUM_PEGS = 3
 
 
 def compute_tower_hanoi(num_rings: int) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    ans = []
+    def helper(n, from_pole, to_pole, other_pole):
+        if n == 1:
+            # just move it to the destination
+            ans.append([from_pole, to_pole])
+        else:
+            # 1 move the top ones (n-1) to the other pole
+            helper(n-1, from_pole, other_pole, to_pole)
+            # 2 move the bottom one to the destination 
+            ans.append([from_pole, to_pole])
+            # 3 move the top ones to the destination 
+            helper(n-1, other_pole, to_pole, from_pole)
+    helper(num_rings, 0, 1, 2)
+    return ans
+    
+    
+    # P0         P1         P2
+    # -         
+    # --
+    # ---
+    # ----
+    # -----
+    # ------
+    
+    # P0          P1         P2
+                # -         
+                # --
+                # ---
+                # ----
+                # -----
+                # ------  
+
+    # if n == 1:
+    #     return [[0,1]]
+    # if n == 2:
+    #     return [[0,2],[0,1],[2,1]]
+    # if n == 3:
+    #     return [[0,1],[0,2],[1,2],[0,1],[2,0],[2,1],[0,1]]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 @enable_executor_hook
