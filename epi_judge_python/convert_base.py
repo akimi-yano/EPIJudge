b936005bdfd2fa7c6f2dd55b2bd7ca4618d0133e
Akimi Yano <akimi.mimi.yano@gmail.com>
2020-12-24 18:45:59 -0800

    epi

--- epi_judge_python/convert_base.py
+++ epi_judge_python/convert_base.py
@@ -2,8 +2,33 @@ from test_framework import generic_test
 
 
 def convert_base(num_as_string: str, b1: int, b2: int) -> str:
-    # TODO - you fill in here.
-    return ''
+    base_str_to_int = {  '0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7,
+                    '8': 8, '9': 9, 'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15    }
+    int_to_base_str =  {  0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7',
+                    8: '8', 9: '9', 10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F'    }
+    
+    # convert form b1 to base 10
+    digit = 0
+    b10_total = 0
+    negative = False
+    if num_as_string[0] == '-':
+        num_as_string = num_as_string[1:]
+        negative = True
+    for char in reversed(num_as_string):
+        b10_total += base_str_to_int[char] * b1 ** digit 
+        digit += 1
+    
+    # convert from base 10 to b2 
+    b2_total = ""
+    while b10_total:
+        quotient, remainder = divmod(b10_total, b2)
+        b2_total = int_to_base_str[remainder] + b2_total
+        b10_total = quotient
+    if len(b2_total) == 0:
+        b2_total = '0'
+    if negative:
+        b2_total = '-' + b2_total
+    return b2_total
 
 
 if __name__ == '__main__':
--- epi_judge_python/replace_and_remove.py
+++ epi_judge_python/replace_and_remove.py
@@ -5,10 +5,56 @@ from test_framework import generic_test
 from test_framework.test_utils import enable_executor_hook
 
 
-def replace_and_remove(size: int, s: List[str]) -> int:
-    # TODO - you fill in here.
-    return 0
+# This solution works - creating a new array
+# def replace_and_remove(size: int, s: List[str]) -> int:
+#     ans = []
+#     for i in range(size):
+#         c = s[i]
+#         if c  == 'a':
+#             ans.append('d')
+#             ans.append('d')
+#         elif c == 'b':
+#             pass
+#         else:
+#             ans.append(c)
+            
+#     for i in range(len(ans)):
+#         s[i] = ans[i]
+#     return len(ans)
 
+# This solution works - in place
+def replace_and_remove(size: int, s: List[str]) -> int:
+    # use write_idx and read_idx
+    # size is the number of elements you read
+    
+    # remove b
+    write_idx = 0
+    final_size = 0
+    for read_idx in range(size):
+        if s[read_idx] == 'b':
+            continue
+        s[write_idx] = s[read_idx]
+        write_idx += 1
+        final_size += 1
+        if s[read_idx] == 'a':
+            final_size += 1
+    while len(s) < final_size:
+        s.append(None)
+    # print(size, s)
+    # print(final_size, write_idx)
+    
+    new_write_idx = final_size-1
+    # iterate from the back and replace a with 2 ds
+    for read_idx in range(write_idx-1, -1, -1):
+        if s[read_idx] == 'a':
+            s[new_write_idx] = 'd'
+            new_write_idx -= 1
+            s[new_write_idx] = 'd'
+            new_write_idx -= 1
+        else:
+            s[new_write_idx] = s[read_idx]
+            new_write_idx -= 1
+    return final_size 
 
 @enable_executor_hook
 def replace_and_remove_wrapper(executor, size, s):
