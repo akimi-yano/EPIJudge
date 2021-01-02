e36bdb6e55a279478518e0c46b7b33020f231647
Akimi Yano <akimi.mimi.yano@gmail.com>
2021-01-01 16:58:46 -0800

    added chess

--- epi_judge_python/n_queens.py
+++ epi_judge_python/n_queens.py
@@ -4,8 +4,24 @@ from test_framework import generic_test
 
 
 def n_queens(n: int) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    # n is # of queens and matrix of size n * n
+    def helper(arr):
+        nonlocal n
+        if len(arr) == n:
+            ans.append(arr)
+            return 
+        banned = set(arr)
+        nextidx = len(arr)
+        for i in range(len(arr)):
+            banned.add(arr[i] + (nextidx - i))
+            banned.add(arr[i] - (nextidx - i))
+        for num in range(n): 
+            if not arr or (arr and (abs(num - arr[-1]) >= 2) and num not in banned):
+                helper(arr + [num])
+            
+    ans = []
+    helper([])
+    return ans
 
 
 def comp(a, b):
