0190cf169637665f4e722dfd4db1b4ad8ab124af
Akimi Yano <akimi.mimi.yano@gmail.com>
2021-01-03 13:59:34 -0800

    palindrome

--- epi_judge_python/is_string_palindromic_punctuation.py
+++ epi_judge_python/is_string_palindromic_punctuation.py
@@ -1,23 +1,41 @@
 from test_framework import generic_test
 
+# This solution works but needs optimization
+# def is_palindrome(s: str) -> bool:
+#     valid = set("123456789abcdefghijklmnopqrstuvwxyz")
+#     s = s.lower()
+#     news = ""
+#     for c in s:
+#         if c in valid:
+#             news += c
+#     left = 0
+#     right = len(news)-1
+#     while left < right:
+#         if news[left] != news[right]:
+#             return False
+#         left += 1
+#         right -= 1    
+#     return True
 
+
+# This solution works !
+# Time: O(N)
+# Space: O(1)
 def is_palindrome(s: str) -> bool:
-    valid = set("123456789abcdefghijklmnopqrstuvwxyz")
-    s = s.lower()
-    news = ""
-    for c in s:
-        if c in valid:
-            news += c
-    left = 0
-    right = len(news)-1
+    left, right = 0, len(s)-1
     while left < right:
-        if news[left] != news[right]:
+        if not s[left].isalnum():
+            left += 1
+        elif not s[right].isalnum(): 
+            right -= 1
+        elif s[left].lower() != s[right].lower():
             return False
-        left += 1
-        right -= 1    
+        else:
+            left += 1
+            right -= 1    
     return True
-
-
+    
+    
 if __name__ == '__main__':
     exit(
         generic_test.generic_test_main(
