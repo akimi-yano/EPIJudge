00a1f2430a2966c3fff06cde9839e040266895ba
Akimi Yano <akimi.mimi.yano@gmail.com>
2021-01-14 21:21:21 -0800

    done

--- epi_judge_python/matrix_connected_regions.py
+++ epi_judge_python/matrix_connected_regions.py
@@ -4,9 +4,16 @@ from test_framework import generic_test
 
 
 def flip_color(x: int, y: int, image: List[List[bool]]) -> None:
-    # TODO - you fill in here.
-    return
-
+    def helper(row, col, prev_color):
+        if not (0 <= row <= len(image)-1) or not (0 <= col <= len(image[0])-1) or image[row][col] != prev_color:
+            return 
+        image[row][col] = image[row][col] ^ 1
+        helper(row, col+1, prev_color)
+        helper(row, col-1, prev_color)
+        helper(row+1, col, prev_color)
+        helper(row-1, col, prev_color)
+    
+    helper(x,  y, image[x][y])
 
 def flip_color_wrapper(x, y, image):
     flip_color(x, y, image)
