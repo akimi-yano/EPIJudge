c6e0ddf1d1bce172d4d0aa41a2650269a878ad87
Akimi Yano <akimi.mimi.yano@gmail.com>
2021-01-14 22:15:27 -0800

    bit

--- epi_judge_python/reverse_bits.py
+++ epi_judge_python/reverse_bits.py
@@ -1,10 +1,18 @@
 from test_framework import generic_test
 
 
+# This solution works
 def reverse_bits(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    ans = 0
+    for i in range(32):
+        ans |= ((((x & (1<<i)) << 63) >> i) >> i) | ((((x & ((1 << 63) >>i)) << i << i)) >> 63)
+    return ans
 
+# This solution works
+# def reverse_bits(x: int) -> int:
+#     return int(((bin(x)[2:]).zfill(64)[::-1]), base = 2)
+    
+    
 
 if __name__ == '__main__':
     exit(
