26c54dc2188075cc7c81ae2829b015b04f0edf13
Akimi Yano <akimi.mimi.yano@gmail.com>
2021-01-18 14:19:00 -0800

    good problems

--- epi_judge_python/online_median.py
+++ epi_judge_python/online_median.py
@@ -1,11 +1,38 @@
 from typing import Iterator, List
+import heapq
 
 from test_framework import generic_test
 
+# 1, 0, 3, 5, 2, 0, 1
+# maxheap: [1]
+# minheap: []
+def balance_heaps(maxheap, minheap):
+    while len(maxheap) > len(minheap) + 1:
+        elem = -heapq.heappop(maxheap)
+        heapq.heappush(minheap, elem)
+    if len(minheap) < 1:
+        return
+    while -maxheap[0] > minheap[0]:
+        m1 = -heapq.heappop(maxheap)
+        m2 = heapq.heappop(minheap)
+        heapq.heappush(maxheap, -m2)
+        heapq.heappush(minheap, m1)
 
 def online_median(sequence: Iterator[int]) -> List[float]:
-    # TODO - you fill in here.
-    return []
+    ans = []
+    minheap = []
+    maxheap = []
+    for num in sequence:
+        heapq.heappush(maxheap, -num)
+        balance_heaps(maxheap, minheap)
+        if len(maxheap) > len(minheap):
+            median = -maxheap[0]
+            ans.append(median)
+        else:
+            m1 = -maxheap[0]
+            m2 = minheap[0]
+            ans.append((m1+m2) / 2)
+    return ans
 
 
 def online_median_wrapper(sequence):
--- epi_judge_python/tree_with_parent_inorder.py
+++ epi_judge_python/tree_with_parent_inorder.py
@@ -3,10 +3,45 @@ from typing import List
 from binary_tree_with_parent_prototype import BinaryTreeNode
 from test_framework import generic_test
 
-
 def inorder_traversal(tree: BinaryTreeNode) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    if tree is None:
+        return []
+
+    ans = []
+    prev, cur = tree.parent, tree
+    while cur:
+        # if previous node was the parent, we should go left.
+        if prev == cur.parent:
+            # if we can go left
+            if cur.left is not None:
+                prev, cur = cur, cur.left
+            # if we cannot go left
+            else:
+                # add current node to answer
+                ans.append(cur.data)
+                # if we can go right
+                if cur.right is not None:
+                    prev, cur = cur, cur.right
+                # if we cannot go right
+                else:
+                    # go to parent
+                    prev, cur = cur, cur.parent
+        # if previous node was the left, we should go right.
+        elif prev == cur.left:
+            # add current node to answer
+            ans.append(cur.data)
+            # if we can go right
+            if cur.right is not None:
+                prev, cur = cur, cur.right
+            # if we cannot go right
+            else:
+                # go to parent
+                prev, cur = cur, cur.parent
+        # if previous node was the right, we should go to parent.
+        else:
+            # go to parent
+            prev, cur = cur, cur.parent
+    return ans
 
 
 if __name__ == '__main__':
