0aca47cfd6ded4be9996f3ddce0793d91435ec52
Akimi Yano <akimi.mimi.yano@gmail.com>
2021-01-18 16:43:26 -0800

    median

--- epi_judge_python/online_median.py
+++ epi_judge_python/online_median.py
@@ -3,6 +3,9 @@ import heapq
 
 from test_framework import generic_test
 
+
+# THIS SOLUTION WORKS - 2 heaps !
+'''
 # 1, 0, 3, 5, 2, 0, 1
 # maxheap: [1]
 # minheap: []
@@ -33,7 +36,30 @@ def online_median(sequence: Iterator[int]) -> List[float]:
             m2 = minheap[0]
             ans.append((m1+m2) / 2)
     return ans
+'''
 
+# THIS SOLUTION WORKS ! sorted list !!!
+#  0 1 2 3
+# [1 2 3 4]
+#  - - - -
+# 4 // 2 = 2
+# (4 - 1) // 2 = 1
+
+#  0 1 2
+# [1 2 3]
+#  - - -
+# 3 // 2 = 1
+# (3 - 1) // 2 = 1
+from sortedcontainers import SortedList
+def online_median(sequence: Iterator[int]) -> List[float]:
+    ans = []
+    arr = SortedList()
+    for num in sequence:
+        arr.add(num)
+        mid_idx1, mid_idx2 = len(arr)//2, (len(arr)-1)//2
+        median = (arr[mid_idx1] + arr[mid_idx2])/2
+        ans.append(median)
+    return ans
 
 def online_median_wrapper(sequence):
     return online_median(iter(sequence))
