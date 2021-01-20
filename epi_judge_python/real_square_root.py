a1fe468e2e7ccdf4372dbb380bc037f7d08463e1
Akimi Yano <akimi.mimi.yano@gmail.com>
2021-01-19 21:46:40 -0800

    square

--- epi_judge_python/real_square_root.py
+++ epi_judge_python/real_square_root.py
@@ -1,10 +1,36 @@
 from test_framework import generic_test
 
 
-def square_root(x: float) -> float:
-    # TODO - you fill in here.
-    return 0.0
+# This solution works
+# def square_root(x: float) -> float:
+#     return x ** (1/2)
 
+import math
+def square_root(x: float) -> float:
+    if x < 1:
+        left = 0
+        right = 1
+        while left < right:
+            mid = (left + right) / 2
+            if math.isclose(x, mid ** 2, rel_tol=10**(-15)):
+                return mid
+            elif mid ** 2 < x:
+                left = mid
+            else:
+                right = mid
+        return left
+    else:
+        left = 1
+        right = x
+        while left < right:
+            mid = (left + right) /2
+            if math.isclose(mid ** 2, x, rel_tol=10**(-15)):
+                return mid
+            elif mid ** 2 > x:
+                right = mid
+            else:
+                left = mid
+        return left
 
 if __name__ == '__main__':
     exit(
