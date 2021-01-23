7d957a661b202900b375c10b232354b369f962cd
Akimi Yano <akimi.mimi.yano@gmail.com>
2021-01-22 23:14:34 -0800

    bst

--- epi_judge_python/bst_from_preorder.py
+++ epi_judge_python/bst_from_preorder.py
@@ -3,11 +3,32 @@ from typing import List, Optional
 from bst_node import BstNode
 from test_framework import generic_test
 
+# this solution works - important to make the nonlocal variable for idx (dont pass in the recursion)
+# its because the array index should change by using each element and move (not in the recurion but iteration)  
+def rebuild_bst_from_preorder(preorder_sequence: List[int]) -> Optional[BstNode]:
+    if not preorder_sequence:
+        return None
 
-def rebuild_bst_from_preorder(preorder_sequence: List[int]
-                              ) -> Optional[BstNode]:
-    # TODO - you fill in here.
-    return None
+    idx = 0
+    arr = preorder_sequence
+    
+    def helper(left_limit, right_limit):
+        nonlocal idx
+        nonlocal arr
+        if idx >= len(arr) or not left_limit < arr[idx] < right_limit:
+            return None
+
+        node = BstNode(data=arr[idx])
+        idx += 1
+        
+        node.left = helper(left_limit, node.data)
+        node.right = helper(node.data, right_limit)
+        return node
+
+    root = helper(float('-inf'), float('inf'))
+
+    return root
+
 
 
 if __name__ == '__main__':
