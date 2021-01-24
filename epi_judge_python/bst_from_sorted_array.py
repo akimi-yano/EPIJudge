9e39ab3f0ea517df4739a798ef46f6d42f72092f
Akimi Yano <akimi.mimi.yano@gmail.com>
2021-01-23 18:24:18 -0800

    bst

--- epi_judge_python/bst_from_sorted_array.py
+++ epi_judge_python/bst_from_sorted_array.py
@@ -10,8 +10,19 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def build_min_height_bst_from_sorted_array(A: List[int]) -> Optional[BstNode]:
-    # TODO - you fill in here.
-    return None
+    def helper(start, end):
+        # print(start, end)
+        if end - start < 0:
+            return None
+        mid_idx = (end+start)//2
+        node = BstNode(A[mid_idx])
+        node.left = helper(start, mid_idx-1)
+        node.right = helper(mid_idx+1, end)
+        return node
+    
+    if not A:
+        return None
+    return helper(0, len(A)-1)
 
 
 @enable_executor_hook
