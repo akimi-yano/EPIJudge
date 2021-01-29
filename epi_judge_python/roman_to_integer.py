f594ff631fde171b5d8f8292bc549a602cef3322
Akimi Yano <akimi.mimi.yano@gmail.com>
2021-01-28 20:42:46 -0800

    completed

--- epi_judge_python/roman_to_integer.py
+++ epi_judge_python/roman_to_integer.py
@@ -2,9 +2,23 @@ from test_framework import generic_test
 
 
 def roman_to_integer(s: str) -> int:
-    # TODO - you fill in here.
-    return 0
-
+    '''
+    59
+    xxxxxiiiiiiiii
+    lviiii
+    lix
+    '''
+    memo = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+    ans = 0
+    prev = None
+    for elem in s:
+        if prev is None or prev >= memo[elem]:
+            ans += memo[elem]
+        else:
+            ans -= prev
+            ans += (memo[elem] - prev)
+        prev = memo[elem]
+    return ans
 
 if __name__ == '__main__':
     exit(
