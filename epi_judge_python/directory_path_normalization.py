fc3d89f2bc6a2bc5727ca52afd79091c85cd5f63
Akimi Yano <akimi.mimi.yano@gmail.com>
2021-01-29 23:25:09 -0800

    tree

--- epi_judge_python/directory_path_normalization.py
+++ epi_judge_python/directory_path_normalization.py
@@ -2,8 +2,23 @@ from test_framework import generic_test
 
 
 def shortest_equivalent_path(path: str) -> str:
-    # TODO - you fill in here.
-    return ''
+    arr = path.split("/")
+    ans = []
+    head = ""
+
+    if path[0] == "/":
+        head = "/"
+        
+    for elem in arr:
+        if elem == "..":
+            if ans and ans[-1] != "..":
+                ans.pop()
+            else:
+                ans.append(elem)
+        elif elem.isalnum():
+            ans.append(elem)
+
+    return  head + "/".join(ans) 
 
 
 if __name__ == '__main__':
--- epi_judge_python/tree_from_preorder_with_null.py
+++ epi_judge_python/tree_from_preorder_with_null.py
@@ -5,10 +5,40 @@ from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 from test_framework.test_utils import enable_executor_hook
 
-
+# This solution works:
 def reconstruct_preorder(preorder: List[int]) -> BinaryTreeNode:
-    # TODO - you fill in here.
-    return BinaryTreeNode()
+    if not preorder:
+        return None
+
+    def helper():
+        nonlocal i
+        data = preorder[i]
+        i += 1
+        if data is None:
+            return  None
+        node = BinaryTreeNode(data)
+        node.left = helper()
+        node.right  = helper()
+        return node
+    
+    i = 0
+    return helper()
+    
+            
+
+# This solution works:
+# def reconstruct_preorder(preorder: List[int]) -> BinaryTreeNode:
+    
+#     def helper(cur):
+#         nonlocal i
+#         i += 1
+#         if cur.data is None:
+#             return None
+#         cur.left = helper(BinaryTreeNode(preorder[i]))
+#         cur.right = helper(BinaryTreeNode(preorder[i]))
+#         return cur
+#     i = 0
+#     return helper(BinaryTreeNode(preorder[i]))
 
 
 @enable_executor_hook
