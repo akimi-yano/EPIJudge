b60d3fc71fab4f7aa4a41cb954f87454287308f6
Akimi Yano <akimi.mimi.yano@gmail.com>
2021-02-06 23:17:36 -0800

    sqrt

--- epi_judge_python/a_b_sqrt2.py
+++ epi_judge_python/a_b_sqrt2.py
@@ -2,10 +2,30 @@ from typing import List
 
 from test_framework import generic_test
 
-
+import heapq
 def generate_first_k_a_b_sqrt2(k: int) -> List[float]:
-    # TODO - you fill in here.
-    return []
+    seen = set([])
+    a = b = 0
+    minheap = []
+    heapq.heappush(minheap, (a+b*2**(1/2), a, b))
+    ans = []
+    while minheap:
+        val, a, b = heapq.heappop(minheap)
+        if (a, b) in seen:
+            continue
+        seen.add((a, b))
+        
+        if k:
+            ans.append(val)
+            k -= 1
+        else:
+            return ans
+        
+        if (a+1, b) not in seen:
+            heapq.heappush(minheap, ((a+1)+b*2**(1/2), a+1, b))
+        if (a, b+1) not in seen:
+            heapq.heappush(minheap, (a+(b+1)*2**(1/2), a, b+1))
+
 
 
 if __name__ == '__main__':
